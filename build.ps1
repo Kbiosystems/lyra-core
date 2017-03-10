dca22437451a8f638e693dad7896a6d83692a3f9
CicerosPatience <astyves@gmail.com>
2017-03-10 13:26:04 +0000

    changed config to Release

--- build.ps1
+++ build.ps1
@@ -6,6 +6,6 @@ $binfo = "$($env:LIB_VERSION)$(".")$($env:APPVEYOR_BUILD_NUMBER)"
 
 Write-Host "Assembly version info" $binfo
 
-dotnet build Lyra.Core /p:Version=$binfo
+dotnet build -c Release Lyra.Core /p:Version=$binfo
 
-dotnet pack Lyra.Core --include-symbols /p:Version=$env:APPVEYOR_BUILD_VERSION
+dotnet pack Lyra.Core -c Release --no-build --include-symbols /p:Version=$env:APPVEYOR_BUILD_VERSION
