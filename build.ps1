d76b6bf740c098d6079233413983f1365f6515af
CicerosPatience <astyves@gmail.com>
2017-03-10 13:06:58 +0000

    addressing build version info

--- build.ps1
+++ build.ps1
@@ -2,6 +2,10 @@ dotnet --version
 
 dotnet restore Lyra.Core
 
-dotnet build Lyra.Core
+$binfo = "$($env:LIB_VERSION)$(".")$($env:APPVEYOR_BUILD_NUMBER)"
+
+Write-Host "Assembly version info" $binfo
+
+dotnet build Lyra.Core /p:Version=$binfo
 
 dotnet pack Lyra.Core --include-symbols /p:Version=$env:APPVEYOR_BUILD_VERSION
