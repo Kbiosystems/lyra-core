64992c101b863d9cfb1c5b4609cf7f166ddaddf4
Andrew St. Yves <astyves@gmail.com>
2017-03-10 10:29:34 +0000

    Update appveyor.ps1

--- appveyor.ps1
+++ appveyor.ps1
@@ -4,11 +4,11 @@
 
 Write-Host "Downloading latest .NET Core SDK..."
 
-(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=841686', "$env:appveyor_build_folder\dotnet-core-sdk.exe")
+(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=841686','dotnet-core-sdk.exe')
 # Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=841686" -OutFile "dotnet-core-sdk.exe"
 
 Write-Host "Installing .NET Core SDK..."
 
-Invoke-Command -ScriptBlock { $env:appveyor_build_folder\dotnet-core-sdk.exe /S /v/qn }
+Invoke-Command -ScriptBlock { ./dotnet-core-sdk.exe /S /v/qn }
 
 Write-Host "Installation succeeded." -ForegroundColor Green
