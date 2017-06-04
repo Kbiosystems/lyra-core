493233f3299f63ad6002b970c8506a0ca6e8ee10
Andrew St. Yves <astyves@gmail.com>
2017-06-04 16:02:40 +0100

    Merge pull request #3 from CicerosPatience/code documentation
    * updated can message recieve event documentation
    
    * add comments for input/output events and handlers
    
    * finished documenting events, changed come conepts
    
    * code documentation (mostly) done

--- LyraElectronics.Core/Events/CanMessageRecievedEventHandler.cs
+++ LyraElectronics.Core/Events/CanMessageRecievedEventHandler.cs
@@ -4,5 +4,17 @@ using System.Text;
 
 namespace LyraElectronics.Events
 {
+    /// <summary>
+    ///     Delegate for CAN message recieved inside the <see cref="ICanController"/> imlpementation.
+    /// </summary>
+    /// <param name="sender">
+    ///     The object invoking the event.
+    /// </param>
+    /// <param name="message">
+    ///     The <see cref="CanMessage"/> recieved.
+    /// </param>
+    /// <remarks>
+    ///     The <see cref="CanBoard"/> uses this event to parse <see cref="CanMessage"/> results.
+    /// </remarks>
     public delegate void CanMessageRecievedEventHandler(object sender, CanMessage message);
 }
--- LyraElectronics.Core/MioBoard.cs
+++ LyraElectronics.Core/MioBoard.cs
@@ -4,14 +4,33 @@ using System.Text;
 
 namespace LyraElectronics
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="LyraElectronics.CanBoard" />
     public class MioBoard : CanBoard
     {
+        /// <summary>
+        ///     An Int32 representation of hexadecimal addressing 
+        ///     range of the board.
+        /// </summary>
         internal override int Range { get { return 0x500; } }
 
-        public MioBoard(int address, ICanController controller) 
-            : base(address, controller)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MioBoard"/> class.
+        /// </summary>
+        /// <param name="sequenceNumber"></param>
+        /// <param name="controller"></param>
+        public MioBoard(int sequenceNumber, ICanController controller) 
+            : base(sequenceNumber, controller)
         { }
 
+        /// <summary>
+        ///     Parse the specified CAN data
+        /// </summary>
+        /// <param name="data">
+        ///     The data to parse.
+        /// </param>
         protected internal override void Parse(byte[] data)
         {
             throw new NotImplementedException();
