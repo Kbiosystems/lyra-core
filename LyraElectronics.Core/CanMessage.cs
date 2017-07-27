6377098f4cefe594c1273675aedd8ea0d38c510d
Andrew St. Yves <astyves@gmail.com>
2017-07-27 21:45:36 +0100

    Merge pull request #10 from CicerosPatience/CanMessage Parsing
    Added basic CanMessage Parse method

--- LyraElectronics.Core/CanMessage.cs
+++ LyraElectronics.Core/CanMessage.cs
@@ -67,5 +67,28 @@ namespace LyraElectronics
             return Convert.ToString(Address, 16) + " " + Convert.ToString(DataLength, 16) + " " + string.Join(" ", Data.Select(d => Convert.ToString(d, 16)));
         }
 
+        /// <summary>
+        ///     Converts the btye[] representation of a 
+        ///     can message to a <see cref="CanMessage"/> 
+        ///     object equivalent.
+        /// </summary>
+        /// <param name="bytes">
+        ///     The byte array to parse.
+        /// </param>
+        /// <returns>
+        ///     The parsed <see cref="CanMessage" /> object.
+        /// </returns>
+        public static CanMessage Parse(byte[] bytes)
+        {
+            int id = int.Parse(Encoding.ASCII.GetString(bytes.SubArray(0, 3)), System.Globalization.NumberStyles.HexNumber);
+            int length = int.Parse(Encoding.ASCII.GetString(bytes.SubArray(3, 1)), System.Globalization.NumberStyles.HexNumber);
+            List<byte> data = new List<byte>();
+            for (int j = 4; j < 4 + (length * 2); j += 2)
+            {
+                data.Add(byte.Parse(Encoding.ASCII.GetString(bytes.SubArray(j, 2)), System.Globalization.NumberStyles.HexNumber));
+            }
+
+            return new CanMessage(id, length, data.ToArray());
+        }
     }
 }
