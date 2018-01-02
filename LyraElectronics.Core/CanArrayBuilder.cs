d886fe842c568b5bcef6b328ae8a86c64e612b96
CicerosPatience <astyves@gmail.com>
2018-01-02 13:14:01 +0000

    started frameworking CanArray object and builder

---
+++ LyraElectronics.Core/CanArrayBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LyraElectronics
+{
+    public class CanArrayBuilder
+    {
+        public CanController Controller { get; set; }
+
+        public CanArrayBuilder AddSaciaBoard(int address)
+        {
+            return this;
+        }
+        
+        public CanArrayBuilder AddDynamicSaciaBoard(int initialAddress, int[] availableAddresses)
+        {
+            return this;
+        }
+
+        public CanArrayBuilder AddDynamicSaciaBoard(int initialAddress, int[] availableAddresses, Action<int, int> onSequenceNumberChanged)
+        {
+            return this;
+        }
+
+        public CanArray Build()
+        {
+            return new CanArray();
+        }
+    }
+}
